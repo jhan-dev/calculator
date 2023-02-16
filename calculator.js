e44bac9c75c06c99780d98ddd2fe6d86231bf940
jhan-dev <jhan.technology@gmail.com>
2023-02-15 16:09:10 -0800

    Addition feature added

--- calculator.js
+++ calculator.js
@@ -1,8 +1,22 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+
 const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function(req, res) {
-    res.send("Hello World!!!")
+    // res.send("Hello World!")
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", function(req, res){
+    // res.send("Thanks for posting that!")
+let num1 = Number(req.body.n1)
+let num2 = Number(req.body.n2)
+
+let result = num1 + num2
+
+    res.send(`The result of the calculation is ${result}`)
 })
 
 app.listen(3000, function(){
